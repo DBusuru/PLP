669b5289d8da950f7426b4d2ac0f87f5eaaf0ad3
David Busuru <davidbusuru1@gmail.com>
2024-11-02 15:10:00 +0300

    Intro to python assignment

---
+++ user_input.py
@@ -0,0 +1,13 @@
+def get_name(name):
+    name = input("Enter your name: ")
+    return name
+
+def get_age(age):
+    age = input("Enter your age: ")
+    return age
+
+def get_location(location):
+    location = input("Enter your location: ")
+    return location
+
+print(f"Hello {get_name(get_name)}, You are {get_age(get_age)} years old, and Live in {get_location(get_location)}")
